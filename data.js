3eba04a491bf990e291078df84e00de59c1bb78c
Md. Abdullah Sheikh <abdullahshaike500@gmail.com>
2026-08-28 22:17:32 +0600

    Add subfolders for Government and Jama-govt-sel

--- data.js
+++ data.js
@@ -50,6 +50,95 @@ const sealData = [
         name_bn: "সরকারি সিল",
         name_en: "Government",
         icon: "fa-solid fa-landmark",
-        subfolders: []
+        subfolders: [
+            {
+                id: "dhaka",
+                name_bn: "ঢাকা",
+                name_en: "Dhaka",
+                icon: "fa-solid fa-location-dot",
+                subfolders: [
+                    {
+                        id: "adv-mukbul",
+                        name_bn: "অ্যাডভোকেট মোঃ মুকবুল হোসেন",
+                        name_en: "Adv. Md. Mukbul Hossain",
+                        icon: "fa-solid fa-user-tie",
+                        images: [
+                            "images/Notary-Public/mizen/1000753544-removebg-preview.png",
+                            "images/Notary-Public/mizen/1000753546-removebg-preview.png",
+                            "images/Notary-Public/mizen/1000753542-removebg-preview.png"
+                        ]
+                    }
+                ]
+            },
+            {
+                id: "chattogram",
+                name_bn: "চট্টগ্রাম",
+                name_en: "Chattogram",
+                icon: "fa-solid fa-location-dot",
+                subfolders: [
+                    {
+                        id: "adv-mukbul",
+                        name_bn: "অ্যাডভোকেট মোঃ মুকবুল হোসেন",
+                        name_en: "Adv. Md. Mukbul Hossain",
+                        icon: "fa-solid fa-user-tie",
+                        images: [
+                            "images/Notary-Public/mizen/1000753544-removebg-preview.png",
+                            "images/Notary-Public/mizen/1000753546-removebg-preview.png",
+                            "images/Notary-Public/mizen/1000753542-removebg-preview.png"
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: "Jama-govt-sel",
+        name_bn: "জামা-সরকারি-সিল",
+        name_en: "Government",
+        icon: "fa-solid fa-landmark",
+        subfolders: [
+            {
+                id: "dhaka",
+                name_bn: "ঢাকা",
+                name_en: "Dhaka",
+                icon: "fa-solid fa-location-dot",
+                subfolders: [
+                    {
+                        id: "adv-mukbul",
+                        name_bn: "অ্যাডভোকেট মোঃ মুকবুল হোসেন",
+                        name_en: "Adv. Md. Mukbul Hossain",
+                        icon: "fa-solid fa-user-tie",
+                        images: [
+                            "images/Notary-Public/mizen/1000753544-removebg-preview.png",
+                            "images/Notary-Public/mizen/1000753546-removebg-preview.png",
+                            "images/Notary-Public/mizen/1000753542-removebg-preview.png"
+                        ]
+                    }
+                ]
+            },
+            {
+                id: "chattogram",
+                name_bn: "চট্টগ্রাম",
+                name_en: "Chattogram",
+                icon: "fa-solid fa-location-dot",
+                subfolders: [
+                    {
+                        id: "adv-mukbul",
+                        name_bn: "অ্যাডভোকেট মোঃ মুকবুল হোসেন",
+                        name_en: "Adv. Md. Mukbul Hossain",
+                        icon: "fa-solid fa-user-tie",
+                        images: [
+                            "images/Notary-Public/mizen/1000753544-removebg-preview.png",
+                            "images/Notary-Public/mizen/1000753546-removebg-preview.png",
+                            "images/Notary-Public/mizen/1000753542-removebg-preview.png"
+                        ]
+                    }
+                ]
+            }
+        ]
     }
+
+
+
+    
 ];
